34494166a0c484a7a752c1588315aff3332aa57c
julianne-vela <cjvela@outlook.com>
2021-02-16 15:53:07 -0800

    updated scroll styling and removed local sorting

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -4,7 +4,7 @@ import './SideBar/SideBar.css';
 import './Pokemon/PokemonList.css';
 import request from 'superagent';
 // import pokeData from '../../data.js';
-import AppliedFilters from './AppliedFilters.js';
+import MarqueeScroll from './MarqueeScroll.js';
 import PokemonList from './Pokemon/PokemonList';
 import SideBar from './SideBar/SideBar.js';
 
@@ -15,49 +15,60 @@ export default class SearchPage extends Component {
         sortBy: ['pokemon', 'type_1', 'attack', 'defense', 'hp'],
         sortSelected: 'pokemon',
         tFilterSelected: 'all',
+        loading: false,
     }
 
     componentDidMount = async () => {
+        await this.fetchPokemon()
+    }
+
+    fetchPokemon = async () => {
+        this.setState({
+            loading: true,
+            pokeData: [],
+        })
+
         const pokeData = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex')
 
         this.setState({
+            loading: false,
             pokeData: pokeData.body.results
         })
     }
 
-    sortAsc = () => {
-        const { pokeData, sortSelected } = this.state;
-        this.setState({
-            pokeData: [...pokeData].sort((a, b) => {
-                if (a[sortSelected] > b[sortSelected]) {
-                    return 1;
-                }
+    // sortAsc = () => {
+    //     const { pokeData, sortSelected } = this.state;
+    //     this.setState({
+    //         pokeData: [...pokeData].sort((a, b) => {
+    //             if (a[sortSelected] > b[sortSelected]) {
+    //                 return 1;
+    //             }
 
-                if (a[sortSelected] < b[sortSelected]) {
-                    return -1;
-                }
+    //             if (a[sortSelected] < b[sortSelected]) {
+    //                 return -1;
+    //             }
 
-                return 0;
-            })
-        });
-    };
+    //             return 0;
+    //         })
+    //     });
+    // };
 
-    sortDesc = () => {
-        const { pokeData, sortSelected } = this.state;
-        this.setState({
-            pokeData: [...pokeData].sort((a, b) => {
-                if (a[sortSelected] > b[sortSelected]) {
-                    return -1;
-                }
+    // sortDesc = () => {
+    //     const { pokeData, sortSelected } = this.state;
+    //     this.setState({
+    //         pokeData: [...pokeData].sort((a, b) => {
+    //             if (a[sortSelected] > b[sortSelected]) {
+    //                 return -1;
+    //             }
 
-                if (a[sortSelected] < b[sortSelected]) {
-                    return 1;
-                }
+    //             if (a[sortSelected] < b[sortSelected]) {
+    //                 return 1;
+    //             }
 
-                return 0;
-            })
-        });
-    };
+    //             return 0;
+    //         })
+    //     });
+    // };
 
     handleQueryChange = (e) => {
         this.setState({
@@ -112,7 +123,7 @@ export default class SearchPage extends Component {
                 />
 
                 {/* Applied Filters - options stretch */}
-                < AppliedFilters className='applied-filters' />
+                < MarqueeScroll className='marquee-scroll' />
 
                 {/* PokeList Module */}
                 < PokemonList className='pokemon-list float'
