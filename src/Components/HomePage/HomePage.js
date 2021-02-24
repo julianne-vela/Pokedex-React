508b3f6d402c3d95770c3d0fdd705c5bf7b070dd
Julianne Vela <cjvela@outlook.com>
2021-02-24 01:30:48 -0800

    home page is now tidied up and pokedex is COMPLETE

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -4,15 +4,22 @@ import './HomePage.css';
 export default class HomePage extends Component {
     render() {
         return (
-            <div className='welcome'>
-                <h2>Welcome to Nessi's PokeDex!</h2>
-                <p>
-                    Click Search to start finding your favorite pokemon!
-                </p>
-                <p>
-                    In the future, we plan to add the ability to save teams of pokemon to your profile so that you can reference back to them when planning your epic battles!
-                </p>
-            </div>
+            <main className='homePage'>
+                <section>
+                    <h2 className='welcome'>
+                        Welcome!
+                    </h2>
+                    <p className='intro'>
+                        Click Search to start finding your  favorite pokemon!
+                    </p>
+                    <p className='future'>
+                        In the future, we plan to add the ability to save teams of pokemon to your profile so that you can reference back to them when planning your epic battles!
+                    </p>
+                    <p className='signOff'>
+                        Now go Catch 'em all!
+                    </p>
+                </section>
+            </main>
         )
     }
 }
